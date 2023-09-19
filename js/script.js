a23fd6698c33fa0ec7fffaffd8030772a29e8944
aldoaquino451 <138690750+aldoaquino451@users.noreply.github.com>
2023-09-19 13:24:10 +0200

    [add]  generate bomb random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/*
+
+  0. Alla funzione generate inserisco il comando che assegna un ID numerico ad ogni quadrato
+  1. Costruisco una Funzione random con intervallo - 1 - e - valore massimo delle celle - (va quindi inserita dopo la scelta delle dimensioni della griglia e deve partire al click del button dello start)
+  2. Creo un ciclo do while in cui il computer
+      1. sceglierà 16 valori casuali
+      2. saranno valori univoci
+      3. ogni numero viene pushato dentro un array BOMBS
+  3. al click sul quadrato controlliamo se il valore dell' ID è incluso nell'array bombs
+  4. aggiungiamo la classe bomb e si colora di rosso
+  5. se non è una bomba la prtita continua e si colora di blue 
+  6. ogni volta che si colora di blue il counter auemnta di uno
+
+*/
 
 // Richiamiamo il container in js e lo resetto 
 const container = document.querySelector('.container');
@@ -12,19 +26,27 @@ const btn = document.querySelector('button')
 // Dichiaro la variabile che determinerà la dimensione della griglia
 let squareSize;
 
+// Dichiaro un array che conserva l'id numerico delle bombe
+const bombArr = [];
+generateBomb(selectSize.value ** 2) ; 
+console.log(bombArr);
+
 
 // Al click parte la funzione handleclick
 btn.addEventListener('click', handleClick);
 
 
-// Funzione che resette il container e viene generata la griglia in base all'opzione scelta
+// Funzione che resette il container 
+// 
+// generata la griglia in base all'opzione scelta
 function handleClick() {
 
   reset();
 
   squareSize = selectSize.value;
 
-  generate(squareSize);
+  generateSquare(squareSize);
+
 }
 
 
@@ -36,18 +58,23 @@ function reset() {
 
 
 // Funzione per la creazione della griglia con ciclo for
-// Creo un elemento con classe square che collego al container
-// Modifico la variabile custom css che determina width e height dei quadrati 
+// Creo un elemento con classe square 
+// Lo collego al container
+// Imposto la dimensione 
+// Inserisco il comando che assegna un ID numerico ad ogni quadrato
 // Parte la funzione toggleClicked
-function generate(size) {
+function generateSquare(size) {
   
   for ( let i = 1; i <= size * size; i++ ) {
 
     const square = document.createElement('div');
     square.className = 'square';
+
     container.append(square);
     
     square.style.setProperty("--number-of-square", size);
+    
+    square._numberID = i;
 
     toggleClicked(square, i);  
 
@@ -66,7 +93,48 @@ function toggleClicked(element, index) {
     element.innerHTML = `
     <span>${index}</span>
     `;
+
+    console.log(element)
     
   });
 };
+
+
+// Funzione che genera 16 numeri random
+// Creo un ciclo do while in cui il computer
+//   1. sceglierà i numeri con la funzione random
+//   2. saranno valori univoci
+//   3. ogni numero viene pushato dentro un array BOMBS
+function generateBomb(maxRandom) {
+
+  for ( let i = 1; i <= 10; i++ ) {
+     
+    let bombNumb;
+
+    do {
+      
+      bombNumb = randomizer(1, maxRandom);
+
+      // if ( !bombArr.includes(bombNumb) ) bombArr.push(bombNumb);
+      // else bombNumb = randomizer(1, maxRandom)
+
+
+      // console.log(bombNumb) /* */
+      // console.log(bombArr) /* */
+      
+    }
+    while (!bombArr.includes(bombNumb));
+
+    bombArr.push(bombNumb)
+
+  }
+
+}
+
+
+// Funzione numero randon con min e max
+function randomizer(min, max) {
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
